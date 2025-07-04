d3451d5b45260d3f03644f3407903c818820f040
Robin <robinsalim@yahoo.com>
2025-07-04 12:56:52 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  CLIENT_ID: '777842538509-b30eqog08ovtacuqvdh7g4oe8ej2c7k0.apps.googleusercontent.com',
+  SHEET_ID: '1bVnJwShGvmGR5W5NZ3vBSeKFhlWuzC8ej13lY3kldSY',
+  API_KEY: '',
+  RANGE: 'Sheet1!A1:D10'
+};
