4e97fb076ffa3952cb12c2e3978426e1f7b70a76
Robin <robinsalim@yahoo.com>
2025-07-04 13:36:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   API_KEY: 'AIzaSyBACPe1smZqQgmw5CvMTvRUlN4nRwjPMJw',
 
   USERS_SHEET_ID: '1bVnJwShGvmGR5W5NZ3vBSeKFhlWuzC8ej13lY3kldSY',
-  USERS_RANGE: 'Users!A2:D',
+  USERS_RANGE: 'Sheet1!A2:D',
 
   ADMINS_SHEET_ID: '12Y-6WjdC9HGpfKTUPQKfBGuhnq7iPcf1q6ZOg-xRXPw',
   ADMINS_RANGE: 'Sheet1!B2:C' // Username in B, Password in C
