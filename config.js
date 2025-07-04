45f8cadfdda15befd96a5480da9e2665d3069633
Robin <robinsalim@yahoo.com>
2025-07-04 13:21:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   CLIENT_ID: '777842538509-b30eqog08ovtacuqvdh7g4oe8ej2c7k0.apps.googleusercontent.com',
-  API_KEY: '',
+  API_KEY: 'AIzaSyBACPe1smZqQgmw5CvMTvRUlN4nRwjPMJw',
 
   USERS_SHEET_ID: '1bVnJwShGvmGR5W5NZ3vBSeKFhlWuzC8ej13lY3kldSY',
   USERS_RANGE: 'Users!A2:D',
