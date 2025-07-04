e447a696fa5d8b1634b22fb7f865f73ee993c70f
Robin <robinsalim@yahoo.com>
2025-07-04 13:44:21 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -8,3 +8,22 @@ export function initAuth(onReady) {
     onReady();
   });
 }
+
+export function authorize(callback) {
+  const tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: CONFIG.CLIENT_ID,
+    scope: 'https://www.googleapis.com/auth/spreadsheets.readonly',
+    callback
+  });
+  tokenClient.requestAccessToken({ prompt: 'consent' });
+  return tokenClient;
+}
+
+export function logout(tokenClient) {
+  const token = tokenClient?.credentials?.access_token;
+  if (token) {
+    google.accounts.oauth2.revoke(token, () => {
+      console.log('Logged out');
+    });
+  }
+}
