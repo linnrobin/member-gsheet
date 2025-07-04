01ce234c08f484df6670ef7ecbbfb77a2dcee0df
Robin <robinsalim@yahoo.com>
2025-07-04 13:11:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
+// config.js
 const CONFIG = {
   CLIENT_ID: '777842538509-b30eqog08ovtacuqvdh7g4oe8ej2c7k0.apps.googleusercontent.com',
-  SHEET_ID: '1bVnJwShGvmGR5W5NZ3vBSeKFhlWuzC8ej13lY3kldSY',
   API_KEY: '',
-  RANGE: 'Sheet1!A1:D10'
+
+  USERS_SHEET_ID: '1bVnJwShGvmGR5W5NZ3vBSeKFhlWuzC8ej13lY3kldSY',
+  USERS_RANGE: 'Users!A2:D',
+
+  ADMINS_SHEET_ID: '12Y-6WjdC9HGpfKTUPQKfBGuhnq7iPcf1q6ZOg-xRXPw',
+  ADMINS_RANGE: 'Sheet1!A2:B', // Assumes username in A, password in B
 };
