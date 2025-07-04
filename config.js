78e82aaea42da6385619155e6eba051d07dd6358
Robin <robinsalim@yahoo.com>
2025-07-04 14:01:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ export const CONFIG = {
   USERS_SHEET_ID: '1bVnJwShGvmGR5W5NZ3vBSeKFhlWuzC8ej13lY3kldSY',
   ADMINS_SHEET_ID: '12Y-6WjdC9HGpfKTUPQKfBGuhnq7iPcf1q6ZOg-xRXPw',
   USERS_RANGE: 'Sheet1!A2:D',
-  ADMINS_RANGE: 'Sheet1!B2:C'
+  ADMINS_RANGE: 'Sheet1!A2:D'
 };
